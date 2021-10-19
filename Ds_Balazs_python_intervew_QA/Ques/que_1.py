2981b28d55c3ce157be6f530067e0496b5f61060
Balakumar <bala93kumar@gmail.com>
2021-10-19 14:09:00 +0530

    queue completed

--- Ds_Balazs_python_intervew_QA/Ques/que_1.py
+++ Ds_Balazs_python_intervew_QA/Ques/que_1.py
@@ -7,7 +7,7 @@ class Queue:
     def __init__(self):
         self.first = None
         self.last = None
-        self.size = None
+        self.size = 0
 
     def enque(self, data):
 
@@ -23,6 +23,37 @@ class Queue:
         self.size += 1
 
         return
-    
+
     def deque(self):
-        pass
+
+        if self.first is None :
+            return None
+        else  :
+            temp = self.first
+            self.first = self.first.next
+            self.size -= 1
+
+    def printValues(self):
+
+        currentNode = self.first
+
+        while currentNode is not None :
+            print(currentNode.data , end="-->")
+            currentNode  = currentNode.next
+
+
+if __name__ == '__main__':
+
+    q1 = Queue()
+
+    q1.enque(10)
+    q1.enque(11)
+    q1.enque(12)
+
+    q1.printValues()
+
+    q1.deque()
+
+    print(end='\n')
+
+    q1.printValues()
