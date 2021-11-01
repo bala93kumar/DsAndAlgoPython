011798d0b93ed4a42c20c9eb693e22f619005c44
Balakumar <bala93kumar@gmail.com>
2021-11-01 23:27:34 +0530

    reverse doubly linkdlist

--- doubleLinked/doubleList1.py
+++ doubleLinked/doubleList1.py
@@ -7,18 +7,41 @@ def printList(head):
         print(currentNode.data , end=' ')
         currentNode = currentNode.next
 
+def reverse(list):
+
+    temp = None
+    current = list
+
+    while current is not None:
+        temp = current.prev
+        current.prev = current.next
+        current.next = temp
+        current = current.prev
+
+    list = temp.prev
+
+    while list is not None:
+        print(list.data, end=' ')
+        list = list.next
+
+
+
+
 def insertSorted(list,data):
 
     prev = list
     next  = list.next
 
-    # print(prev.__dict__)
-    # print(next.__dict__)
 
     newNode = Node(data)
 
     while next is not None :
-        if data > prev.data and  data < next.data:
+        if data < prev.data:
+            newNode.next = prev
+            prev.prev = newNode
+            list = newNode
+            break
+        elif data > prev.data and  data < next.data or data == next.data or data == prev.data:
             temp = prev.next
             prev.next = newNode
             newNode.next = temp
@@ -26,6 +49,10 @@ def insertSorted(list,data):
         else:
             prev = prev.next
             next = next.next
+
+    if data > prev.data:
+        prev.next = newNode
+        newNode.prev = prev
 
     currentNode = list
     while currentNode is not None:
@@ -66,14 +93,19 @@ if __name__ == '__main__':
 
     list1 = DoubleList()
 
-    list1.insert(1)
+    # list1.insert(1)
+    list1.insert(2)
     list1.insert(3)
-    list1.insert(4)
+    list1.insert(5)
 
     printList(list1.head)
 
     print('\n')
 
 
-    insertSorted(list1.head,2)
+    insertSorted(list1.head,6)
+
+    print('\n')
+
+    reverse(list1.head)
 
